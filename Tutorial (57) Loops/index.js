f289ea117028640da13e129e79ee1ad472b226d3
Shubham Bharati <shubham9214679859@gmail.com>
2026-09-24 12:15:44 +0530

    Add initial website files

---
+++ Tutorial (57) Loops/index.js
@@ -0,0 +1,46 @@
+console.log("I am a Tutorial on Loops")
+// let a = 1;
+// // console.log(a);
+// // console.log(a + 1);
+// // console.log(a + 2);
+// // console.log(a + 3);
+// // console.log(a + 4);
+
+// Five Types of Loops in JavaScript
+/*1. For Loop
+2.For in Loop
+3.For of Loop
+4.While Loop
+5.Do-Whilt Loop */
+
+// let a = 1;
+
+// for (let i = 0; i<100; i++) {
+//     console.log(a+i)
+// }
+// let obj = {
+//     name : "Harry" ,
+//     role : "Programmer" ,
+//     company : "Codewithharry Ai"
+// }
+// for (const key in obj) {      // object ke liye hota hai 
+//     const element = obj[key];
+//   console.log( key,element)
+// }
+// for (const c of "Harry"){  // array ke liye hota hai 
+//     console.log(c)
+// }
+
+
+// let i = 0;
+// while(i<6){
+//     console.log(i)
+//     i++;
+// }
+
+
+let i = 7;
+do {console.log(i)
+    i++;
+    
+} while (i<6);
