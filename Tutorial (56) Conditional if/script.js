6816135b0f17a95fea1726ab897d6fcc8f601565
Shubham Bharati <shubham9214679859@gmail.com>
2026-09-24 12:15:28 +0530

    Add initial website files

---
+++ Tutorial (56) Conditional if/script.js
@@ -0,0 +1,45 @@
+// console.log("Hey i am shubham ")
+
+// let age = 15;
+// if(age>18){
+//     console.log("You can drive");
+// }
+// else{
+//     console.log("You can not drive");
+// }
+// let age = 1;
+// // let grace = 2;
+// // age += grace
+
+// // console.log(age + grace)
+// // console.log(age - grace)
+// // console.log(age * grace)
+// // console.log(age / grace)
+// // console.log(age ** grace)
+// // console.log(age % grace) //remender
+
+// // if ((age + grace) > 18) {
+// // if (age != 18) {
+// if (age == 18) {
+//     console.log("You can drive");
+// }
+// else if (age == 0){
+//     console.log("You can Kidding ");
+// }
+// else if (age == 1){
+//     console.log("You can again Kidding ");
+// }
+// else{
+//     console.log("You can not drive")
+// }
+a = 6;
+b = 8;
+let c = a > b ? ( a - b) : (b-a); //ternary statement shortcut
+
+/* translates to:
+if(a>b){
+let c = a-b;
+}
+else {
+    let c = b - a;
+}*/ 
