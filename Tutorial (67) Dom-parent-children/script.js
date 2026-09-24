0ac6506d51180adcdef9faff5bf748731293c7d0
Shubham Bharati <shubham9214679859@gmail.com>
2026-09-24 12:19:31 +0530

    Add initial website files

---
+++ Tutorial (67) Dom-parent-children/script.js
@@ -0,0 +1,35 @@
+  console.log("Hello World")
+
+  /* 
+  cont.firstElementChild
+  <div class= "box1">...</div>
+
+  cont.lastElementChild.ParentElement 
+  <div class="constainer">...</div>
+
+  document.body.firstElementChild.ChildNodes
+  textnodes , commentnodes , etc.
+
+  document.body.firstElementChild.children
+  <div class="box1">...</div> 
+  <div class="box2">...</div> 
+  <div class="box3">...</div> 
+  <div class="box4">...</div> 
+  <div class="box5">...</div> 
+  
+  document.body.firstElementChild.children[3]
+  <div class="box4">...</div>  // kyuki index count hmesha 0 se start hota hai .
+  
+  document.body.firstElementChild.children[3].nextElementSibling
+  <div class="box5">...</div> 
+  
+  document.body.firstElementChild.children[3].previousElementSibling
+  <div class="box3">...</div> 
+  
+  document.body.children[1]
+  <table>...</table>
+
+  document.body.children[1].rows
+  [tr ,tr]
+  
+  */
