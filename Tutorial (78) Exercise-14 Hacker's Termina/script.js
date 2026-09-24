da409e8b68151d34555ac27ccd27db5fc2171546
Shubham Bharati <shubham9214679859@gmail.com>
2026-09-24 12:23:58 +0530

    Add initial website files

---
+++ Tutorial (78) Exercise-14 Hacker's Termina/script.js
@@ -0,0 +1,26 @@
+alert("Welcome To Hacker Zone")
+setTimeout(() => {  // "() =>" = ye function likhane ka short method hai [old method function(){  }] 
+    console.log(" Initializing Hacking.....")
+    alert("Initializing Hacking.....")
+},3000);
+
+setTimeout(() => {  // "() =>" = ye function likhane ka short method hai [old method function(){  }] 
+    console.log("Reading Your Files......")
+    alert("Reading Your Files......")
+},6000);
+setTimeout(() => {  // "() =>" = ye function likhane ka short method hai [old method function(){  }] 
+    console.log("Password files Detected....")
+    alert("Password files Detected....")
+},9000);
+setTimeout(() => {  // "() =>" = ye function likhane ka short method hai [old method function(){  }] 
+    console.log(" Sending all passwords and personal files to server...  ")
+    alert("Sending all passwords and personal files to server...")
+},12000);
+setTimeout(() => {  // "() =>" = ye function likhane ka short method hai [old method function(){  }] 
+    console.log("Cleaning Up.... ")
+    alert("Cleaning Up....")
+},15000);
+setTimeout(() => {  // "() =>" = ye function likhane ka short method hai [old method function(){  }] 
+    console.log("Your Pc is Hacked!.....")
+    alert("Your Pc is Hacked!.....")
+},18000);
