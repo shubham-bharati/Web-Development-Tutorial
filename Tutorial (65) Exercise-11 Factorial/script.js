8c991f67b3319a643fe8977926f78647c3483134
Shubham Bharati <shubham9214679859@gmail.com>
2026-09-24 12:18:44 +0530

    Add initial website files

---
+++ Tutorial (65) Exercise-11 Factorial/script.js
@@ -0,0 +1,10 @@
+/* Write a program to calculate factorial of a number using reduce and using for loops
+6
+6! = 6*5*4*3*2*1
+*/
+let a = 10;
+let product = 1;
+for (let i = 1; i <= a; i++) {
+    product = product * i;
+}
+console.log(product);
