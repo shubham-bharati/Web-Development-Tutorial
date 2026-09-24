bd716e8ba0e7cb885f161d96f3aa1f76ff47059c
Shubham Bharati <shubham9214679859@gmail.com>
2026-09-24 12:17:01 +0530

    Add initial website files

---
+++ Tutorial (61) Solution - 09 Calculator/script.js
@@ -0,0 +1,20 @@
+let random = Math.random()
+let a = prompt("Enter first number")
+let b = prompt("Enter second number")
+let c = prompt("Enter operation")
+let obj = {
+    "+" : "-",
+    "*" : "+",
+    "-" : "/",
+    "/" : "**",
+}
+console.log(random)
+if (random > 0.1){
+    //perform correct calculation
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+}
+else{
+    //perform wrong calculation
+    c = obj[c]
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+}
