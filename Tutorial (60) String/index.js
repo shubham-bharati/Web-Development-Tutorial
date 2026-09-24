6ed6728bfab2d2bd68060359c67e506488b4ce17
Shubham Bharati <shubham9214679859@gmail.com>
2026-09-24 12:16:32 +0530

    Add initial website files

---
+++ Tutorial (60) String/index.js
@@ -0,0 +1,24 @@
+console.log("This is string Tutorial")
+let a = "Harry" ;
+console.log(a.length)
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+
+let real_name ="Harry"
+let friend ="Shubham"
+console.log("His name is " + real_name + " and His firend name is " + friend)
+console.log(`His name is ${real_name} and his friend name is ${friend}`)  // Template latter 
+
+console.log(a.toUpperCase())
+console.log(a.toLowerCase())
+console.log(a.length)
+console.log(a.slice(1,5))
+console.log(a.slice(2))
+console.log(a.replace("Ha","77"))
+let b= "Shubham"
+console.log(a.concat(b))
+console.log(a,"anshu","sudansh")
